93883f9825b646d3cff3f2c41cc6aff42cfb4e00
Tuan Nguyen <tuannguyen@Tuans-MacBook-Pro.local>
2023-07-27 00:31:03 +0700

    Add storybook

---
+++ src/components/auth/header/index.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Header from '~/components/auth/header/index';
+
+const meta: Meta<typeof Header> = {
+    component: Header,
+};
+
+export default meta;
+type Story = StoryObj<typeof Header>;
+
+export const PrimaryHeader: Story = {
+    args: {
+        //👇 The args you need here will depend on your component
+    },
+};
