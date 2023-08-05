ef2692c53d009e2e7e2c84a28e86432965dd7b8b
Hoàng Trang <success010280@gmail.com>
2023-08-05 18:58:35 +0700

    fix card course

--- src/components/class/CardClass/index.tsx
+++ src/components/class/CardClass/index.tsx
@@ -17,24 +17,29 @@ export const CardClass: React.FC<CardCourseProps> = ({
 }) => 
  (
     <Card
-      className = {'tw-md tw-shadow-md hover:tw-cursor-pointer hover:tw-bg-gray-100 tw-overflow-hidden'}  
-      style = {{ width: 360, border: '1px solid #D8DCF0'}}
+      className = {'tw-shadow-md tw-max-w-sm hover:tw-cursor-pointer hover:tw-bg-gray-100 tw-overflow-hidden object-contain tw-inline-block'}  
+      style = {{ border: '1px solid #D8DCF0'}}
       cover = {
         <img
-          className={'hover:tw-scale-105 tw-transition tw-duration-500 tw-object-cover'}
+          className={'tw-w-4/5 md:tw-w-72 object-cover hover:tw-scale-105 tw-transition tw-duration-500'}
           alt = {title}
           src = {imgSrc} 
         />
       }
     >
-      <div className="tw-flex">
-           <Meta 
-            className = 'tw-text-left tw-w-full'
+      <div className="tw-flex tw-flex-row tw-flex-wrap tw-relative">
+        <div className="tw-text-left">
+          <Meta 
             title= {title}
-            description= {id}
           />
-          <EllipsisOutlined className ="tw-text-2xl"/>
+          <p className="tw-text-left tw-uppercase"> {id} </p>
         </div>
 
+        <button 
+        className ="tw-absolute tw-right-2 tw-text-2xl tw-w-12 tw-h-12 tw-bg-inherit hover:tw-rounded-full hover:tw-bg-slate-50 tw-border-none hover:tw-cursor-pointer"
+        >
+          <EllipsisOutlined />
+        </button>
+      </div>
     </Card>
 )
