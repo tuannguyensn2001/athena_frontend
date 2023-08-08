bfb4fef6f131b549b40dede32954562a397781af
Hoàng Trang <success010280@gmail.com>
2023-08-08 23:01:44 +0700

    show menu dropdown of cardCourse

--- src/components/class/CardClass/index.tsx
+++ src/components/class/CardClass/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { EllipsisOutlined } from "@ant-design/icons";
-import { Card } from "antd";
+import { EllipsisOutlined, ArrowRightOutlined, EditOutlined, EyeInvisibleOutlined, CopyOutlined, DeleteOutlined } from "@ant-design/icons";
+import type { MenuProps } from 'antd';
+import { Card, Dropdown } from "antd";
 
 const { Meta } = Card;
 
@@ -10,12 +11,38 @@ type CardCourseProps = {
     id: string,
 }
 
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key?: React.Key | null,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem('Vào lớp', '1', <ArrowRightOutlined />),
+  getItem('Chỉnh sửa', '2', <EditOutlined />),
+  getItem('Ẩn lớp', '3', <EyeInvisibleOutlined />),
+  getItem('Tạo bản sao lớp', '4', <CopyOutlined />),
+  getItem('Xoá', '5', <DeleteOutlined />),
+  
+]
+
 export const CardClass: React.FC<CardCourseProps> = ({
     imgSrc,
     title,
     id
 }) => 
- (
+ ( 
+  <div>
     <Card
       className = {'tw-shadow-md tw-max-w-sm hover:tw-cursor-pointer hover:tw-bg-gray-100 tw-overflow-hidden object-contain tw-inline-block'}  
       style = {{ border: '1px solid #D8DCF0'}}
@@ -34,12 +61,16 @@ export const CardClass: React.FC<CardCourseProps> = ({
           />
           <p className="tw-text-left tw-uppercase"> {id} </p>
         </div>
-
-        <button 
-        className ="tw-absolute tw-right-2 tw-text-2xl tw-w-12 tw-h-12 tw-bg-inherit hover:tw-rounded-full hover:tw-bg-slate-50 tw-border-none hover:tw-cursor-pointer"
-        >
-          <EllipsisOutlined />
-        </button>
+        <Dropdown menu={{ items }} trigger={['click']} placement="bottomRight">
+          <button 
+          className ="tw-absolute tw-right-2 tw-text-2xl tw-w-12 tw-h-12 tw-bg-inherit hover:tw-rounded-full hover:tw-bg-slate-50 tw-border-none hover:tw-cursor-pointer"
+          onClick={(e) => e.preventDefault()}
+          >
+            <EllipsisOutlined />
+          </button>
+        </Dropdown>
       </div>
     </Card>
+
+  </div>
 )
