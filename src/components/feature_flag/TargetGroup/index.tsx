577e5cb5b849315521d9565fb7effd5c5ffeec83
Tuan Nguyen <devpro2001@gmail.com>
2023-11-15 19:28:45 +0700

    update

--- src/components/feature_flag/TargetGroup/index.tsx
+++ src/components/feature_flag/TargetGroup/index.tsx
@@ -1,9 +1,22 @@
 import { Button } from 'antd';
+import { AxiosError } from 'axios';
+import { useQuery } from 'react-query';
 import { useToggle } from 'react-use';
 import { ModalTargetGroup } from '~/components/feature_flag/ModalTargetGroup';
+import API from '~/config/network';
+import { ITargetGroup } from '~/models/ITargetGroup';
+import { AppResponse } from '~/types/app';
 
 export function TargetGroup() {
     const [on, toggle] = useToggle(false);
+
+    const { data } = useQuery<AppResponse<ITargetGroup[]>, AxiosError>({
+        queryKey: 'target_group',
+        queryFn: async () => {
+            const response = await API.get('/api/v1/feature_flag/target_group');
+            return response.data;
+        },
+    });
 
     return (
         <div>
