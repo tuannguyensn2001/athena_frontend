5f3e38c2cf64850a1ce5dd361e27fb00ce1c97a4
tuannguyensn2001 <devpro2001@gmail.com>
2023-11-15 05:43:02 -0800

    update

--- src/components/feature_flag/TargetGroup/index.tsx
+++ src/components/feature_flag/TargetGroup/index.tsx
@@ -1,28 +1,66 @@
-import { Button } from 'antd';
-import { AxiosError } from 'axios';
+import { Button, Table } from 'antd';
+import type { AxiosError } from 'axios';
 import { useQuery } from 'react-query';
 import { useToggle } from 'react-use';
 import { ModalTargetGroup } from '~/components/feature_flag/ModalTargetGroup';
 import API from '~/config/network';
-import { ITargetGroup } from '~/models/ITargetGroup';
-import { AppResponse } from '~/types/app';
+import type { ITargetGroup } from '~/models/ITargetGroup';
+import type { AppResponse } from '~/types/app';
+import { useMemo } from 'react';
+import get from 'lodash/get';
 
 export function TargetGroup() {
     const [on, toggle] = useToggle(false);
 
-    const { data } = useQuery<AppResponse<ITargetGroup[]>, AxiosError>({
+    const { data, isLoading } = useQuery<
+        AppResponse<ITargetGroup[]>,
+        AxiosError
+    >({
         queryKey: 'target_group',
         queryFn: async () => {
-            const response = await API.get('/api/v1/feature_flag/target_group');
+            const response = await API.get(
+                '/api/v1/feature_flag/target_groups',
+            );
             return response.data;
         },
     });
+
+    const targetGroups = useMemo(() => get(data, 'data', []), [data]);
 
     return (
         <div>
             <ModalTargetGroup open={on} onToggle={toggle} />
 
             <Button onClick={toggle}>Create</Button>
+
+            <div>
+                <Table
+                    loading={isLoading}
+                    dataSource={targetGroups}
+                    columns={[
+                        {
+                            key: 'id',
+                            dataIndex: 'id',
+                            title: 'Id',
+                        },
+                        {
+                            key: 'name',
+                            dataIndex: 'name',
+                            title: 'Name',
+                        },
+                        {
+                            key: 'target_type',
+                            dataIndex: 'target_type',
+                            title: 'Target type',
+                        },
+                        {
+                            key: 'action',
+                            title: 'Action',
+                            render: (_, record) => <Button>Edit</Button>,
+                        },
+                    ]}
+                />
+            </div>
         </div>
     );
 }
